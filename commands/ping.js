64c5c8fddd630a794069f00192962d0a58fddaa4
TrueXRetro <dragonminner867@outlook.com>
2019-02-13 19:52:34 +0100

    Updated code

--- commands/ping.js
+++ commands/ping.js
@@ -1,19 +1,17 @@
-module.exports.run = async (bot, message, args) => { // This is what will run when the command is called.
+module.exports.run = async (bot, message, args) => { 
 
     //Ping / Pong command
-
-    // We can delete the IF statement since the command handler does that.
  
     
-        message.channel.send({embed:{ // The start of the embed is usually the same, and it looks like this.
+        message.channel.send({embed:{ 
             title:"Ping!",
             description:"Pong!",
-            color: 0x5DADE2   // The colors can be found in the description, remember to put a 0x before all of the numbers.
-        }}) // A link of the objects you can put in the embed will be in the description.
+            color: 0x5DADE2   
+        }}) 
 
     }
 
 
-module.exports.config = { // This is the config for the command, you can add things to this such as proper usage, ect.
+module.exports.config = { 
     command: "ping"
 }
